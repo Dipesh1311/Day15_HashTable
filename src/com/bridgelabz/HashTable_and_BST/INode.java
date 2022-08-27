f0fce879a3e27d7dfd77d2a11e6a1a29e1198eb4
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-08-27 19:45:24 +0530

    Added implemented program for UC1.

---
+++ src/com/bridgelabz/HashTable_and_BST/INode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.HashTable_and_BST;
+
+public interface INode<K> {
+    K getKey();
+
+    void setKey(K key);
+
+    INode<K> getNext();
+
+    void setNext(INode<K> next);
+
+}
+
